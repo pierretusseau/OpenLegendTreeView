4224b0ae87e3581114fab078b420dcd9351d663a
Pierre Tusseau <pierre.tusseau@gmail.com>
2017-01-12 05:35:53 +0100

    Changement d'état des skills + Sélection de skill + Reset des skill

--- src/components/SkillTree.js
+++ src/components/SkillTree.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 
 import Skill from "./SkillTree/Skill.js";
+import './SkillTree.css';
 
 class SkillTree extends Component {
 
 	getSkillsByCategory(array, categoryName) {
-		return array.filter(a => a.category === categoryName).map(skill => <Skill name={skill.name} key={skill.id} skillAvaible={skill.avaible} />);
+		return array.filter(a => a.category === categoryName).map(skill => <Skill name={skill.name} key={skill.id} avaible={skill.avaible} selected={skill.selected} isSkillSelected={this.props.isSkillSelected}/>);
 	}
 
   render() {
 		const skillList = this.props.skills;
     return (
 			<div>
-				{this.getSkillsByCategory(skillList,'General')}
-				{this.getSkillsByCategory(skillList,'Agility')}
+				<div id="NoPrerequisTable" className="SkillTable">
+					<h2>No prerequis</h2>
+					{this.getSkillsByCategory(skillList,'No Prerequis')}
+				</div>
+				<div id="PhysicalTable" className="SkillTable">
+					<h2>Physical</h2>
+					{this.getSkillsByCategory(skillList,'Agility')}
+				</div>
+
 			</div>
     );
   }
--- src/components/SkillTree/Skill.js
+++ src/components/SkillTree/Skill.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
+
 import './Skill.css';
 
 
 class Skill extends Component {
 
-	skillClass(skillState) {
-		if (skillState) {
+	skillClass(skillAvaible,skillSelected) {
+		if ((skillAvaible === true) && (skillSelected === false)) {
 			return("skill--block avaible")
+		} else if ((skillAvaible === true) && (skillSelected === true)) {
+			return("skill--block selected")
 		} else {
 			return ("skill--block")
 		}
 	}
 
+	selectSkill() {
+		// console.log("Button : AddToCart");
+		if(this.props.selected === false) {
+			this.props.isSkillSelected(this.props.name);
+		}
+  }
+
   render() {
     return (
-			<div className={this.skillClass(this.props.skillAvaible)}>
-				<div className="skill--block-wrapper">
-					<h3>Nom : {this.props.name}</h3>
-				</div>
+			<div className="skill">
+				<button
+					className={this.skillClass(this.props.avaible, this.props.selected)}
+					onClick={() => this.selectSkill()}
+				>
+					<div className="skill--block-wrapper">
+						<h3>{this.props.name}</h3>
+					</div>
+				</button>
 			</div>
     );
   }
